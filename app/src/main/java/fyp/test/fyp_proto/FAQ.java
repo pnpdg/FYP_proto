12e7e3382571c1c0187252015684ad599f5535d2
ENJOYDEPROCESS <87604989+ENJOYDEPROCESS@users.noreply.github.com>
2023-05-09 14:28:30 +0800

    1. Latest latest version

--- app/src/main/java/fyp/test/fyp_proto/FAQ.java
+++ app/src/main/java/fyp/test/fyp_proto/FAQ.java
@@ -1,12 +1,26 @@
 package fyp.test.fyp_proto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FAQ extends BaseActivity {
+    ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+
+        backBtn = findViewById(R.id.left_icon);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),ChooseFunction.class));
+                finish();
+            }
+        });
     }
 }
